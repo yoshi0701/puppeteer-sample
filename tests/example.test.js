d73524e9213372482fb95db21ed8f39c5a883d76
tomoaki yoshioka <tomoaki.yoshiki@gmail.com>
2020-07-10 00:35:49 +0900

    fix: use reusable part

--- tests/example.test.js
+++ tests/example.test.js
@@ -1,6 +1,10 @@
 const puppeteer = require('puppeteer')
 const expect = require('chai').expect
 
+const click = require('../lib/helpers')
+const getText = require('../lib/helpers')
+const getCount = require('../lib/helpers')
+
 describe('First test', () => {
   it('should launch the browser', async function() {
     // const browser = await puppeteer.launch({ headless: false})
@@ -13,10 +17,15 @@ describe('First test', () => {
     const title = await page.title()
     const url = await page.url()
     // find selector
-    const text = await page.$eval('h1', element => element.textContent)
+    // const text = await page.$eval('h1', element => element.textContent)
+    const text = await getText(page, 'h1')
+    const count = await getCount(page, 'p')
 
     expect(title).to.be.a('string', 'Example Domain')
 
+
+    await page.goto('hhtp://zero.webappssecurity.com/index.html')
+    await click(page, '#single_button')
     await browser.close()
   })
 })
